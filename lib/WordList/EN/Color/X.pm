c7510c1fc02407f9d52b57ac7a076d11dd1af4ed
perlancar (on pc-home) <perlancar@gmail.com>
2020-06-07 08:22:03 +0700

    Initial commit

---
+++ lib/WordList/EN/Color/X.pm
@@ -0,0 +1,23 @@
+package WordList::EN::Color::X;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use WordList;
+our @ISA = qw(WordList);
+
+# STATS
+
+1;
+# ABSTRACT: Color names from Graphics::ColorNames::X
+
+=head1 SEE ALSO
+
+L<Graphics::ColorNames::X>
+
+=cut
+
+__DATA__
+# COMMAND: list-color-names X | sortsub asciibetically
